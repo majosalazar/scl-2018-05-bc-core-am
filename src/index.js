0cf138c66e019616a07e0d36255a92fbd7a75613
mariajosesalazar <mariaj.salazarf@gmail.com>
2018-05-25 13:09:27 -0400

    cambie configuraciones en los input de texto y numero, y detalles en los botones

--- src/index.js
+++ src/index.js
@@ -1,2 +1,38 @@
 /* Acá va tu código correspondiente al DOM*/
+window.onload = () => {
+    const bEncode = document.getElementById("encode");
+    const bDecode = document.getElementById("decode");
+    const bDisplacement = document.getElementById("displacement");
+
+    //boton encode esta conectado
+    bEncode.addEventListener("click", function() {
+        alert("funciono!");
+    });
+
+    //botón decode esta conectado 
+    bDecode.addEventListener("click", function() {
+        alert("yo tambien funciono!");
+    });
+
+    //boton de desplacamiento 
+    bDisplacement.addEventListener("input", function() {
+        alert("tengo tu número");
+    })
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
 
