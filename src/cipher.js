2262b42c161f1a3f8bc5791d526ea1d0119d49b4
mariajosesalazar <mariaj.salazarf@gmail.com>
2018-05-31 09:49:10 -0400

    volvi a la forma inicial de orden en el index.js ya que al declararla al inicio no me funcionaban, conecte al DOM los resultados del cifrado, y funciona pero no realiza los espacios

--- src/cipher.js
+++ src/cipher.js
@@ -7,17 +7,33 @@ window.cipher = {
     let resultEncode = "";
     //recorre el texto
     for (let i = 0; i < newText.length; i++) {
+      //nueva variable texto en mayuscula
+      let textM = newText.toUpperCase();
       //convierte la letra a numero ascii y la guarda
-      let codeAscii = newText.charCodeAt(i);
-      //ejecuta la operacion matematica de desplazamiento
+      let codeAscii = textM.charCodeAt(i);
+      //ejecuta la operacion matematica de desplazamiento y la guarda
       let textDis = ((codeAscii - 65 + parseInt(displacement)) % 26) + 65;
       //guarda el resultado de cada una y la vuelve a convertir en letra
       resultEncode += String.fromCharCode(textDis);     
     }
+    //devuelve el resultado
     return resultEncode;
 
   },
   decode: (newText, displacement) => {
-    /* Acá va tu código */
+    //aqui guardara el texto
+    let resultDecode = "";
+    //recorre el texto
+    for (let i = 0; i < newText.length; i++) {
+      //nueva variable texto en mayuscula
+      let textM = newText.toUpperCase();
+      //convierte la letra a numero ascii y la guarda
+      let codeAscii = textM.charCodeAt(i);
+      //ejecuta la operacion matematica de desplazamiento --- el ultimo numero debe cambiar 
+      let textDis = ((codeAscii - 65 - parseInt(displacement)) % 26) + 65;
+      //guarda el resultado de cada una y la vuelve a convertir en letra
+      resultEncode += String.fromCharCode(textDis);     
+    }
+    return resultDecode;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
 /* Acá va tu código correspondiente al DOM*/
 window.onload = () => {
-    //declarando const
-    const newText = document.getElementById("textIn").value;
+    //declarando const, si las declaro antes no me funciona :C
+    /*
+    const newText = document.getElementById("textIn").value.toUpperCase();
     const displacement = document.getElementById("numberDis").value;
     const bEncode = document.getElementById("encode");
     const bDecode = document.getElementById("decode");
+    */
 
 
     //el boton reacciona al click y toma los valores de los const del area de texto y clave
     //boton cifrar
     const OnEncode = document.getElementById("btnEncode").addEventListener("click", () => {
         //area de texto + mayusculas
-        document.getElementById("textIn").value.toUpperCase();
+        const newText = document.getElementById("textIn").value.toUpperCase();
         // numero de desplazamientos
-        document.getElementById("numberDis").value;
-        //conectar con el cipher js
-        let textEncode = window.cipher.encode;        //imprime el texto cifrado
-        document.getElementById("resultText").innerHTML = "hola ";
+        const displacement = document.getElementById("numberDis").value;
+        //conectar con el cipher js, trae el resultad
+        let textEncode = window.cipher.encode(newText, displacement);     
+        //imprime el texto cifrado
+        document.getElementById("resultText").innerHTML = "hola " + textEncode;
     })
     //boton decifrar
     const OnDecode = document.getElementById("btnDecode").addEventListener("click", () => {
         //area de texto + mayusculas
-        document.getElementById("textIn").value.toUpperCase();
+        const newText = document.getElementById("textIn").value.toUpperCase();
         //numero de desplazamientos
-        document.getElementById("numberDis").value;
+        const displacement = document.getElementById("numberDis").value;
         //imprime el texto decifrado
         document.getElementById("resultText").innerHTML = "aqui va el texto decifrado: " + "yo soy la clave: " + displacement + " y yo el texto: " + newText;
     })
